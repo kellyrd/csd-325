d185d74e8e32a0531229cf4d749dac0ab37bbef2
Kelly Dimick <kellyrdimick@gmail.com>
2026-09-07 13:19:21 -0600

    100 Beers created

---
+++ module-1/100beers.py
@@ -0,0 +1,12 @@
+# Kelly Dimick
+# 09/07/2026
+# Assignment 1.3
+
+# Input from user for number of beers
+# X bottles of beer on the wall, X bottles of beer, take one down and pass it around, 
+# Remove 1 beer (X = X - 1)
+# X bottles of beer on the wall.
+# Is X greater than 0
+# If x greater than 0 repeat
+# If x is not greater than 0
+# Time to buy more beer

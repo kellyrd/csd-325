9f2ba48e1565606a2299676f3835b022802f9cdb
Kelly Dimick <kellyrdimick@gmail.com>
2026-09-07 14:53:54 -0600

    Update 100beers.py

--- module-1/100beers.py
+++ module-1/100beers.py
@@ -2,6 +2,8 @@
 # 09/07/2026
 # Assignment 1.3 100 Bottles of Beer
 
+# This program asks the user for number of Bottles of Beer (1-100) on the wall and then it counts down to zero, then informing user more beer is needed.
+
 while True:
     try:
         bottles = int(input("How many bottles of beer? "))
